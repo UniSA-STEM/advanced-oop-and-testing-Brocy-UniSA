e1da2604d2ec0ddd3616294a78c526cb36682aa8
Corey BROOKE <brocy076@mymail.unisa.edu.au>
2025-11-10 14:23:06 +1100

    feat: set up basic structure for staff.py
    - created abstract Staff class in staff.py
    - added Zookeeper and Veterinarian subclasses
    - added placeholder feed_animal(), clean_enclosures(), and conduct_animal_health_check() methods

--- staff.py
+++ staff.py
@@ -7,3 +7,33 @@ Username: Brocy076
 This is my own work as defined by the
 University's Academic Misconduct Policy.
 """
+
+from abc import ABC, abstractmethod
+
+
+class Staff(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def do_stuff(self):  # temp function name
+        pass
+
+    def feed_animal(self):
+        pass
+
+    def clean_enclosures(self):
+        pass
+
+    def conduct_animal_health_check(self):
+        pass
+
+
+class Zookeeper(Staff):
+    def do_stuff(self):
+        pass
+
+
+class Veterinarian(Staff):
+    def do_stuff(self):
+        pass
