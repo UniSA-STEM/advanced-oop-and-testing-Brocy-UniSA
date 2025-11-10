69fa1929fef1bdaba8427b51747d15d8f5e1f8f0
Corey BROOKE <brocy076@mymail.unisa.edu.au>
2025-11-10 14:03:45 +1100

    docs: added header docstrings for initial setup
    - added header docstrings for project files
    - testing commit message for new project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-'''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
-This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+File: main.py
+Description: <Temporary Description>.
+Author: Corey Brooke
+ID: 110480857
+Username: Brocy076
+This is my own work as defined by the
+University's Academic Misconduct Policy.
+"""
