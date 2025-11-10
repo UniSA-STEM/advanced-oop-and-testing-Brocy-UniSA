8006e00d059389a4ccd5f90d7eebc4b3a93945cb
Corey BROOKE <brocy076@mymail.unisa.edu.au>
2025-11-10 14:23:48 +1100

    feat: set up basic structure for enclosure.py
    - added placeholder initialization values

--- enclosure.py
+++ enclosure.py
@@ -7,3 +7,10 @@ Username: Brocy076
 This is my own work as defined by the
 University's Academic Misconduct Policy.
 """
+
+
+class Enclosure:
+    def __init__(self, size, environmental_type, cleanliness_level):
+        self.size = size
+        self.environmental_type = environmental_type
+        self.cleanliness_level = cleanliness_level
