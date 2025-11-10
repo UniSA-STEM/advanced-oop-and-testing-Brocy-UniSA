c7e5dfef536197b4ca93ec2aec60a56589095719
Corey BROOKE <brocy076@mymail.unisa.edu.au>
2025-11-10 14:15:58 +1100

    feat: set up basic structure for animal.py
    - created abstract Animal class
    - added Bird, Mammal, and Reptile subclasses
    - set up placeholder installation values
    - added placeholder eat(), sleep(), and make_sound() methods to each subclass

--- animal.py
+++ animal.py
@@ -7,3 +7,67 @@ Username: Brocy076
 This is my own work as defined by the
 University's Academic Misconduct Policy.
 """
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self, name, species, age, dietary_needs):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.dietary_needs = dietary_needs
+
+    @abstractmethod
+    def eat(self):
+        pass
+
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+    @abstractmethod
+    def sleep(self):
+        pass
+
+
+class Bird(Animal):
+    def __init__(self, name, species, age, dietary_needs):
+        super().__init__(name, species, age, dietary_needs)
+
+    def eat(self):
+        pass
+
+    def make_sound(self):
+        pass
+
+    def sleep(self):
+        pass
+
+
+class Mammal(Animal):
+    def __init__(self, name, species, age, dietary_needs):
+        super().__init__(name, species, age, dietary_needs)
+
+    def eat(self):
+        pass
+
+    def make_sound(self):
+        pass
+
+    def sleep(self):
+        pass
+
+
+class Reptile(Animal):
+    def __init__(self, name, species, age, dietary_needs):
+        super().__init__(name, species, age, dietary_needs)
+
+    def eat(self):
+        pass
+
+    def make_sound(self):
+        pass
+
+    def sleep(self):
+        pass
